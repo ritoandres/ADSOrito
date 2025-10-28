c313506783b4f63781841a8d4fed36ab1669da7a
rito <ritoandres01@gmail.com>
2025-10-27 21:36:10 -0500

    asis

--- asincronizmo.js
+++ asincronizmo.js
@@ -31,7 +31,7 @@ async function fetchnewdata() {
 
 fetch("https://jsonplaceholder.typicode.com/posts")
   .then((response) => response.json())
-  .then((data) => console.log(data));*/
+  .then((data) => console.log(data));*
 
   function sendHTTPRequest(method, url, data) {
   return fetch(url, {
@@ -52,4 +52,140 @@ async function fecthPosts() {
   );
   console.log(responseData);
 }
-fecthPosts();
+fecthPosts();*
+
+const listElement = document.querySelector(".posts");
+const postTemplate = document.getElementById("single-post");
+const form = document.querySelector("#new-post form");
+const fetchButton = document.querySelector("#available-posts button");
+const postList = document.querySelector("#posts-container");
+
+function sendHTTPRequest(method, url, data) {
+  return fetch(url, {
+    method: method,
+    body: JSON.stringify(data),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  }).then((response) => {
+    return response.json();
+  });
+}
+
+async function fecthPosts() {
+  const responseData = await sendHTTPRequest(
+    "GET",
+    "https://jsonplaceholder.typicode.com/posts"
+  );
+  console.log(responseData);
+  const listOfPosts = responseData;
+
+  for (const post of listOfPosts) {
+    const postContainer = document.createElement("article");
+    postContainer.id = post.id;
+    postContainer.classList.add("post-item");
+
+    const title = document.createElement("h2");
+    title.textContent = post.title;
+
+    const body = document.createElement("p");
+    body.textContent = post.body;
+
+    const button = document.createElement("button");
+    button.textContent = "DELETE Content";
+
+    postContainer.append(title);
+    postContainer.append(body);
+    postContainer.append(button);
+
+    listElement.append(postContainer);
+  }
+}
+
+
+fetchButton.addEventListener("click", fecthPosts);*/
+
+
+
+const listElement = document.querySelector(".posts");
+const postTemplate = document.getElementById("single-post");
+const form = document.querySelector("#new-post form");
+const fetchButton = document.querySelector("#available-posts button");
+const postList = document.querySelector("#posts-container");
+
+function sendHTTPRequest(method, url, data) {
+  return fetch(url, {
+    method: method,
+    body: JSON.stringify(data),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  }).then((response) => {
+    return response.json();
+  });
+}
+
+async function fecthPosts() {
+  const responseData = await sendHTTPRequest(
+    "GET",
+    "https://jsonplaceholder.typicode.com/posts"
+  );
+  console.log(responseData);
+  const listOfPosts = responseData;
+
+  for (const post of listOfPosts) {
+    const postContainer = document.createElement("article");
+    postContainer.id = post.id;
+    postContainer.classList.add("post-item");
+
+    const title = document.createElement("h2");
+    title.textContent = post.title;
+
+    const body = document.createElement("p");
+    body.textContent = post.body;
+
+    const button = document.createElement("button");
+    button.textContent = "DELETE Content";
+
+    postContainer.append(title);
+    postContainer.append(body);
+    postContainer.append(button);
+
+    listElement.append(postContainer);
+  }
+}
+fetchButton.addEventListener("click", fecthPosts);
+
+function createPost(title, content) {
+  const userId = Math.random();
+  const post = { 
+    title: title,
+    body: content,
+    userId: userId
+   };
+
+    sendHTTPRequest(
+      "POST",
+      "https://jsonplaceholder.typicode.com/posts",
+      post
+    );
+}
+   form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const enteredTitle = event.currentTarget.querySelector("#title").value;
+    const enteredContent = event.currentTarget.querySelector("#content").value;
+    createPost(enteredTitle, enteredContent);
+    console.log(enteredTitle, enteredContent);
+   });
+
+   postList.addEventListener("click", (event) => {
+    if (event.target.tagName === "BUTTON") {
+      const postId = event.target.closest("article").id;
+      console.log("deleting posttwith id" + postId);
+      sendHTTPRequest(
+        "DELETE",
+        `https://jsonplaceholder.typicode.com/posts/${postId}`
+      );
+      event.target.closest("article").remove();
+    }
+   });
